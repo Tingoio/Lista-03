4c479c5bcfd1ed8651ece2998cfe06c443bb5e26
Tingoio <tingoiogamer@gmail.com>
2024-04-03 10:33:22 -0300

    Inicio do exercício13

---
+++ Exercicio13.java
@@ -0,0 +1,9 @@
+public class Exercicio13 {
+    public static void executar(){
+
+        int qtdPessoas = Prompt.lerInteiro("Digite o numero de pessoas que deseja validar: ");
+
+        
+    }
+    
+}
---
+++ classes/Exercito.java
@@ -0,0 +1,19 @@
+package classes;
+
+public class Exercito {
+    public String nome;
+    public char sexo;
+    public int idade;
+    public char saude;
+
+    public Exercito(String nome, char sexo, int idade, char saude){
+        this.nome = nome;
+        this.sexo = sexo;
+        this.idade = idade;
+        this.saude = saude;
+    }
+
+    public String validaAptidão(){
+        
+    }
+}
