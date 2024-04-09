9abaf98c6293c84a28690b71d38468002160aa3f
Tingoio <tingoiogamer@gmail.com>
2024-04-09 15:30:54 -0300

    Exercicio13

--- Exercicio13.java
+++ Exercicio13.java
@@ -1,7 +1,35 @@
+import classes.PessoaApta;
+
 public class Exercicio13 {
     public static void executar(){
 
         int qtdPessoas = Prompt.lerInteiro("Digite o numero de pessoas que deseja validar: ");
+        int aptos = 0, naoAptos = 0;
+
+        for(int i = 0; i < qtdPessoas; i++){
+            String nome = Prompt.lerLinha("Digite o nome da pessoa: ");
+            char sexo = Prompt.lerCaractere("Digite o sexo da pessoa (M/F): ");
+            int idade = Prompt.lerInteiro("Digite a idade da pessoa: ");
+            char saude = Prompt.lerCaractere("A pessoa tem aglum problema de saúde crônico (S/N)? ");
+
+            PessoaApta objPessoaApta = new PessoaApta(nome, sexo, idade, saude);
+
+            if(sexo=='m'||sexo=='M'){
+                if(idade>=18){
+                    if(saude=='s'||saude=='S'){
+                        aptos++;
+                    }else{
+                        naoAptos++;
+                    }
+                }else{
+                    naoAptos++;
+                }
+            }else{
+                naoAptos++;
+            }
+        }
+
+        Prompt.imprimir("Total de pessoas: "+qtdPessoas+"\nAptos: "+aptos+"\nNão aptos: "+naoAptos);
 
         
     }
--- classes/PessoaApta.java
+++ classes/PessoaApta.java
@@ -1,19 +1,15 @@
 package classes;
 
-public class Exercito {
+public class PessoaApta {
     public String nome;
     public char sexo;
     public int idade;
     public char saude;
 
-    public Exercito(String nome, char sexo, int idade, char saude){
+    public PessoaApta(String nome, char sexo, int idade, char saude){
         this.nome = nome;
         this.sexo = sexo;
         this.idade = idade;
         this.saude = saude;
-    }
-
-    public String validaAptidão(){
-        
     }
 }
