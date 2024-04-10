cf2f92c45b020a6a9575dca30cefa74f79cd32d4
Tingoio <tingoiogamer@gmail.com>
2024-04-10 19:27:33 -0300

    Att-Exercicio04

--- classes/Conversao.java
+++ classes/Conversao.java
@@ -2,15 +2,28 @@ package classes;
 
 public class Conversao {
     
-    public double dolar;
-    public double cotacao;
+    private double dolar;
+    private double cotacao;
 
-    public Conversao(double dolar, double cotacao){
+    public Conversao(){
+
+    }
+
+    public double getDolar() {
+        return dolar;
+    }
+
+    public void setDolar(double dolar) {
         this.dolar = dolar;
+    }
+
+    public double getCotacao() {
+        return cotacao;
+    }
+
+    public void setCotacao(double cotacao) {
         this.cotacao = cotacao;
     }
 
-    public double calcConversao(){
-        return dolar * cotacao;
-    }
+    
 }
