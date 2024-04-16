bf015d95efadffd6c3ec099896bf9692ae3b3cdf
Tingoio <tingoiogamer@gmail.com>
2024-04-16 12:42:23 -0300

    Att-Exercicio14

--- Exercicio14.java
+++ Exercicio14.java
@@ -2,6 +2,26 @@ import classes.Produto;
 
 public class Exercicio14 {
     public static void executar(){
+        
+        double somaCusto = 0, somaVenda = 0;
 
+        Produto[] produtos = new Produto[5];
+
+        for(int i = 0; i < 5; i++){
+
+            produtos[i] = new Produto();
+
+            produtos[i].setPrecoCusto(Prompt.lerDecimal("Digite o preco de custo do " + (i+1) + "º produto: "));
+            produtos[i].setPrecoVenda(Prompt.lerDecimal("Digite o preco de venda do " + (i+1) + "º produto: "));
+
+            Prompt.imprimir(produtos[i].calcularResultado(produtos[i].getPrecoVenda(), produtos[i].getPrecoCusto()));
+
+            somaCusto += produtos[i].getPrecoCusto();
+            somaVenda += produtos[i].getPrecoVenda();
+            
+        }
+
+        Prompt.imprimir("A média de custo é de: " + somaCusto / 5);
+        Prompt.imprimir("A média de venda é de: " + somaVenda / 5);
     }
 }
--- classes/Produto.java
+++ classes/Produto.java
@@ -4,8 +4,34 @@ public class Produto {
     private double precoCusto;
     private double precoVenda;
 
-    public Produto(double precoCusto, double precoVenda){
+    public Produto(){
+    }
+
+    public double getPrecoCusto() {
+        return precoCusto;
+    }
+
+    public void setPrecoCusto(double precoCusto) {
         this.precoCusto = precoCusto;
+    }
+
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(double precoVenda) {
         this.precoVenda = precoVenda;
+    }
+
+    public String calcularResultado(double precoVenda, double precoCusto){
+        if(precoVenda-precoCusto<0){
+            return "Prejuízo";
+        }
+        else if(precoVenda-precoCusto>0){
+            return "Lucro";
+        }else{
+            return "Empate";
+        }
+        
     }
 }
